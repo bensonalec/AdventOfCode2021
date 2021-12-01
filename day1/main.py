0c3be078cb491254c309c450fceff2eb080fb574
Alec Benson <alecbenson21@gmail.com>
2021-11-30 22:08:34 -0700

    Finished day 1

--- day1/main.py
+++ day1/main.py
@@ -1,16 +1,34 @@
-with open("dayX.txt") as fi:
-    readIn = fi.read()
+with open("day1.txt") as fi:
+    readIn = fi.readlines()
 
 def preProcess(inp):
-    midPoint = inp.split("\n")
-    output = midPoint
-    return output
+    data = [line.strip() for line in inp]
+    data = [int(x) for x in data if x != ""]
+    return data
 
 def partOne(inp):
-    pass
+    prev = 0
+    incCount = 0
+    for x in inp:
+        if x > prev:
+            incCount += 1
+        prev = x
+    print(incCount-1)
 
 def partTwo(inp):
-    pass
+    prev = 0
+    incCount = 0
+    first = 0
+    last = 3
+    while last <= len(inp):
+        toLook = inp[first:last]
+        cur = sum(toLook)
+        first += 1
+        last += 1
+        if(cur > prev):
+            incCount += 1
+        prev = cur
+    print(incCount - 1)
 
 partOne(preProcess(readIn))
 partTwo(preProcess(readIn))
