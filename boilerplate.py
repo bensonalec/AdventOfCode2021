502a8e65151eea8b76b300dd7029a27e0c4f09ff
Alec Benson <alecbenson21@gmail.com>
2021-11-30 21:36:33 -0700

    first commit

---
+++ boilerplate.py
@@ -0,0 +1,16 @@
+with open("dayX.txt") as fi:
+    readIn = fi.read()
+
+def preProcess(inp):
+    midPoint = inp.split("\n")
+    output = midPoint
+    return output
+
+def partOne(inp):
+    pass
+
+def partTwo(inp):
+    pass
+
+partOne(preProcess(readIn))
+partTwo(preProcess(readIn))
