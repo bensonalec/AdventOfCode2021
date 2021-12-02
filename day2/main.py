a29ff9f3a3f16f7bc2cb6a76bc50926d2da82829
Alec Benson <alecbenson21@gmail.com>
2021-12-01 22:13:12 -0700

    Solved day 2

--- day2/main.py
+++ day2/main.py
@@ -1,16 +1,37 @@
-with open("dayX.txt") as fi:
+with open("day2.txt") as fi:
     readIn = fi.readlines()
 
 def preProcess(inp):
     data = [line.strip() for line in inp]
-    data = [int(x) for x in data if x != ""]
+    data = [x.split(" ") for x in data if x != ""]
+    
     return data
 
 def partOne(inp):
-    pass
+    x = 0
+    y = 0
+    for line in inp:
+        if(line[0] == "forward"):
+            x += int(line[1])
+        if(line[0] == "down"):
+            y += int(line[1])
+        if(line[0] == "up"):
+            y -= int(line[1])
+    print(x * y)
 
 def partTwo(inp):
-    pass
+    x = 0
+    y = 0
+    aim = 0
+    for line in inp:
+        if(line[0] == "forward"):
+            x += int(line[1])
+            y += (aim * int(line[1]))
+        if(line[0] == "down"):
+            aim += int(line[1])
+        if(line[0] == "up"):
+            aim -= int(line[1])
+    print(x * y)
 
 partOne(preProcess(readIn))
 partTwo(preProcess(readIn))
